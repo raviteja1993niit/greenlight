a94865879feff40ac059b7812f7ef5c388b2f6fd
Peng-Yu Chen <me@pengyuc.com>
2023-04-30 22:23:53 -0400

    3.4 Formatting and Enveloping Responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -9,6 +9,8 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type envelope map[string]any
+
 // readIDParam retrieves the "id" URL parameter from the current request context, then converts it
 // to an integer and returns it. If the operation isn't successful, return 0 and an error.
 func (app *application) readIDParam(r *http.Request) (int64, error) {
@@ -28,10 +30,10 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 func (app *application) writeJSON(
 	w http.ResponseWriter,
 	statusCode int,
-	data any,
+	data envelope,
 	headers http.Header,
 ) error {
-	js, err := json.Marshal(data)
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
