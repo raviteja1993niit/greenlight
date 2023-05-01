852087a285606643708853bf14b246fb7dec819b
Peng-Yu Chen <me@pengyuc.com>
2023-04-30 21:10:59 -0400

    2.2 A Basic HTTP Server

---
+++ cmd/api/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+// config holds all the configuration settings for our application.
+type config struct {
+	port int
+	env  string
+}
+
+// application holds the dependencies for out HTTP handlers, helpers, and middleware.
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	logger.Printf("starting %s server on %s", cfg.env, server.Addr)
+	err := server.ListenAndServe()
+	logger.Fatal(err)
+}
