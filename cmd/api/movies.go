23585b93f893ce1d9eae4e44c037e737ef188270
Peng-Yu Chen <me@pengyuc.com>
2023-05-01 21:33:09 -0400

    4.4 Custom JSON Decoding

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -11,10 +11,10 @@ import (
 // createMovieHandler handles requests for "POST /v1/movies".
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
