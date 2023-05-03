3baf3e657f0da8ba69e4a8386425e29d658877de
Peng-Yu Chen <me@pengyuc.com>
2023-05-03 19:06:51 -0400

    7.1 Setting up the Movie Model

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	_ "github.com/lib/pq"
+	"github.com/walkccc/greenlight/internal/data"
 )
 
 const version = "1.0.0"
@@ -31,6 +32,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -64,6 +66,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	mux := http.NewServeMux()
